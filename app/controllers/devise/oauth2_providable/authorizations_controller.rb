df88bbbc2702b9f16fd960c4411a160639668d4b
Joseph Chen <joe1chen@hotmail.com>
2012-03-28 21:18:26 -0700

    Implement auto-approval for users that are already authenticated.

--- app/controllers/devise/oauth2_providable/authorizations_controller.rb
+++ app/controllers/devise/oauth2_providable/authorizations_controller.rb
@@ -29,10 +29,23 @@ module Devise
         end
       end
 
+      def get_valid_refresh_token
+        return current_user.refresh_tokens.where(:client_id => @client.id).not_expired.first if current_user
+        nil
+      end
+
       def authorize_endpoint(allow_approval = false)
         Rack::OAuth2::Server::Authorize.new do |req, res|
           @client = Devise::Oauth2Providable::Client.find_by_client_identifier(req.client_id) || req.bad_request!
           res.redirect_uri = @redirect_uri = req.verify_redirect_uri!(@client.redirect_uri)
+
+          # Auto-approve if we have a valid refresh token.
+          valid_refresh_token = get_valid_refresh_token
+          if valid_refresh_token
+            params[:approve] = true
+            allow_approval = :allow_approval
+          end
+
           if allow_approval
             if params[:approve].present?
               case req.response_type
--- lib/devise/oauth2_providable/models/oauth2_providable.rb
+++ lib/devise/oauth2_providable/models/oauth2_providable.rb
@@ -7,6 +7,7 @@ module Devise
       included do
         has_many :access_tokens, :class_name => 'Devise::Oauth2Providable::AccessToken'
         has_many :authorization_codes, :class_name => 'Devise::Oauth2Providable::AuthorizationCode'
+        has_many :refresh_tokens, :class_name => 'Devise::Oauth2Providable::RefreshToken'
       end
     end
   end
