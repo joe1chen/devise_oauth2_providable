68f6774126ed11e8d46717cd69963490733ba614
Joseph Chen <joe1chen@hotmail.com>
2012-04-16 18:45:40 -0700

    Auto-approve oauth requests.

--- app/controllers/devise/oauth2_providable/authorizations_controller.rb
+++ app/controllers/devise/oauth2_providable/authorizations_controller.rb
@@ -9,7 +9,11 @@ module Devise
       end
 
       def new
-        respond *authorize_endpoint.call(request.env)
+        # Auto-approve request
+        params[:approve] = true
+        respond *authorize_endpoint(:allow_approval).call(request.env)
+        
+        #respond *authorize_endpoint.call(request.env)
       end
 
       def create
@@ -38,13 +42,6 @@ module Devise
         Rack::OAuth2::Server::Authorize.new do |req, res|
           @client = Devise::Oauth2Providable::Client.find_by_client_identifier(req.client_id) || req.bad_request!
           res.redirect_uri = @redirect_uri = req.verify_redirect_uri!(@client.redirect_uri)
-
-          # Auto-approve if we have a valid refresh token.
-          valid_refresh_token = get_valid_refresh_token
-          if valid_refresh_token
-            params[:approve] = true
-            allow_approval = :allow_approval
-          end
 
           if allow_approval
             if params[:approve].present?
