99d0ca56786f86a8e28875d462c99f9e344ac2f9
Robert Dingwell <bobd@mitre.org>
2012-04-24 16:03:55 -0400

    updating gemspec

--- app/models/devise/oauth2_providable/authorization_code.rb
+++ app/models/devise/oauth2_providable/authorization_code.rb
@@ -3,9 +3,5 @@ class Devise::Oauth2Providable::AuthorizationCode
   include Mongoid::Timestamps
   include Devise::Oauth2Providable::ExpirableToken
   store_in :authorization_codes
-
-
-
-  
   expires_according_to :authorization_code_expires_in
 end
--- spec/factories/client_factory.rb
+++ spec/factories/client_factory.rb
@@ -1,5 +1,3 @@
 Factory.define :client, :class => Devise::Oauth2Providable::Client do |f|
   f.name 'test'
-  f.website 'http://localhost'
-  f.redirect_uri 'http://localhost:3000'
 end
