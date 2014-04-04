e6f4d4bdd397880ac1e9ae2ef4bb5202843c9f0b
Joseph Chen <joe1chen@hotmail.com>
2014-04-03 22:26:32 -0700

    Fix spec for auto-approval.

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -11,12 +11,12 @@ describe Devise::Oauth2Providable::AuthorizationsController do
         sign_in user
         get :new, :client_id => client.cidentifier, :redirect_uri => redirect_uri, :response_type => 'code', :use_route => 'devise_oauth2_providable'
       end
-      it { should respond_with :ok }
+      it { should respond_with :found }
       it { should respond_with_content_type :html }
       it { should assign_to(:redirect_uri).with(redirect_uri) }
-      it { should assign_to(:response_type) }
-      it { should render_template 'devise/oauth2_providable/authorizations/new' }
-      it { should render_with_layout 'application' }
+      it { should_not assign_to(:response_type) }
+      it { should_not render_template 'devise/oauth2_providable/authorizations/new' }
+      it { should_not render_with_layout 'application' }
     end
     context 'with invalid redirect_uri' do
       with :user
